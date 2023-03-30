d0acedd49bf9b50189a5ecb9a1c13669b77a8807
Théo Lemaigre <theolemaigrepro@hotmail.com>
2023-03-30 19:03:49 +0200

    Add instances of my component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { FaceSnap } from './models/face-snap.models';
 })
 export class AppComponent implements OnInit{
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   ngOnInit() {
     this.mySnap = new FaceSnap(
@@ -17,6 +19,22 @@ export class AppComponent implements OnInit{
     0,
     'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
     'Une image qui contient un texte alternatif'
-    )
+    );
+    this.myOtherSnap = new FaceSnap(
+      'Three Rock Mountain',
+      'Un endroit magnifique pour les randonnées',
+      new Date(),
+      0,
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/280px-Three_Rock_Mountain_Southern_Tor.jpg',
+      'Une image qui contient une montagne qui est très apprécié des randonneurs'
+    );
+    this.myLastSnap = new FaceSnap(
+      'Le Mont Ventoux',
+      'Le Mont chauve, un lieu de croisade pour de nombreux cyclistes',
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2020/06/05/18/53/mont-ventoux-5264149_960_720.jpg',
+      'Une image qui arbore le Mont Ventoux avec un beau paysage'
+    );
   }
 }
