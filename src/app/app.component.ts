fbbb839976b633be472b472e5daecbb986c5e348
Théo Lemaigre <theolemaigrepro@hotmail.com>
2023-03-31 10:51:55 +0200

    Discover usage of *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,42 @@ import { FaceSnap } from './models/face-snap.models';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
+  /*mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;*/
 
   ngOnInit() {
+    this.faceSnaps = [
+      {
+        title: 'Un test',
+        description: 'la description de mon test',
+        createdDate: new Date(),
+        snaps: 0,
+        imgUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        altImg: 'Une image qui contient un texte alternatif'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées',
+        createdDate: new Date(),
+        snaps: 0,
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/280px-Three_Rock_Mountain_Southern_Tor.jpg',
+        altImg: 'Une image qui contient une montagne qui est très apprécié des randonneurs',
+        location: 'une montagne en Irlande'
+      },
+      {
+        title: 'Le Mont Ventoux',
+        description: 'Le Mont chauve, un lieu de croisade pour de nombreux cyclistes',
+        createdDate: new Date(),
+        snaps: 0,
+        imgUrl: 'https://cdn.pixabay.com/photo/2020/06/05/18/53/mont-ventoux-5264149_960_720.jpg',
+        altImg: 'Une image qui arbore le Mont Ventoux avec un beau paysage',
+        location: 'dans la Drôme Provençale'
+      }
+    ]
+    // Déclaration séparée // Maitenant on passe par un tableau
+    /*
     this.mySnap = new FaceSnap(
     'Un test',
     'la description de mon test',
@@ -38,5 +69,6 @@ export class AppComponent implements OnInit{
       'Une image qui arbore le Mont Ventoux avec un beau paysage',
       'dans la Drôme Provençale'
     );
+    */
   }
 }
